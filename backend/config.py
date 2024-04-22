11337c7f7d8bfb614d9e8f6d9257757bd268545e
Kylan-Thurairajah <kylan.r.thurairajah@gmail.com>
2024-04-22 06:43:42 -0400

    CRUD operations finished

--- backend/config.py
+++ backend/config.py
@@ -7,7 +7,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-app.config["SQL_ALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
